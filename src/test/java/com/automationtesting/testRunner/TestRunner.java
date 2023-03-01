91efea58524038eccf1c27a9fe2618dae6f6efc2
shakthi Nakkeeran <shakthinakkeeran@gmail.com>
2023-03-01 09:50:09 +0530

    update report file

--- src/test/java/com/automationtesting/testRunner/TestRunner.java
+++ src/test/java/com/automationtesting/testRunner/TestRunner.java
@@ -15,22 +15,12 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src\\test\\resources\\functionalTests", glue = "com.automationtesting.stepDefinitions", dryRun = false,
 
-// plugin = { "usage" },
-//
-//plugin = {"com.cucumber.listener.ExtentCucumberFormatter:cucumber-reports/report.html"},
-//"junit:target/cucumber-reports/Cucumber.xml",
-//plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json"},
-plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:cucumber-reports/report.html"},
-
+plugin = { "pretty", "html:target/cucumber-reports" },
 monochrome = true
 )
 class TestRunner {
-	@AfterClass
 	
 	
-	public static void writeExtentReport() {
-		//System.out.println(FileReaderManager.getInstance().getConfigReader().getReportConfigPath());
-		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
-	}
+	
 
 }
